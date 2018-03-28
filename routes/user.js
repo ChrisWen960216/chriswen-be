18c8aaffaee3561c27fa49b69f3aea81ec227162
“ChrisWen960216 <957638221@qq.com>
2018-03-28 09:56:26 +0800

    Add POST user/regiser API

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const { checkLogin } = require('../middlewares/authCheck');
-const { $getPwd } = require('../lib/index');
+const { $getPwd, $registerUser } = require('../lib/index');
 
 const ResponseExtend = require('../extends/response');
 const status = require('../common/status');
@@ -13,26 +13,38 @@ router.get('/', (request, response) => {
   response.end('Hello World');
 });
 
-router.post('/login', (request, response) => {
-  const { user = '', password = '' } = request.body;
+router.post('/register', (request, response) => {
+  const { name, password, authCode } = request.body.user;
   let resData = {};
-  if (user === '' || password === '') {
+  Bcrypt.hashData(password).then((hash) => {
+    const userInfo = { name, password: hash, authCode };
+    return $registerUser(userInfo);
+  }).then((_response) => {
+    resData = ResponseExtend.createResData(status.OPS_SUCCESS, '注册成功', _response);
+    return response.json(resData);
+  }).catch((error) => {
+    resData = ResponseExtend.createResMsg(status.OPS_FAILURE, error);
+    return response.json(resData);
+  });
+});
+
+router.post('/login', (request, response) => {
+  const { name = '', password = '' } = request.body.user;
+  let resData = {};
+  if (name === '' || password === '') {
     resData = ResponseExtend.createResMsg(status.DATA_ILLEGAL, '数据非法');
     return response.json(resData);
   }
 
-  return $getPwd(user)
-    .then((pwd) => {
-      if (!(pwd === null || undefined)) {
-        Bcrypt.confirmData(password, pwd);
-      }
-    })
+  return $getPwd(name)
+    .then(pwd => Bcrypt.confirmData(password, pwd))
     .then((result) => {
       if (result === true) {
         resData = ResponseExtend.createResMsg(status.OPS_SUCCESS, '登陆成功');
         request.session.user = 'ChrisWen';
+      } else {
+        resData = ResponseExtend.createResMsg(status.PWD_ILLEGAL, '用户名或者密码错误');
       }
-      resData = ResponseExtend.createResMsg(status.PWD_ILLEGAL, '用户名或者密码错误');
       return response.json(resData);
     })
     .catch((error) => {
